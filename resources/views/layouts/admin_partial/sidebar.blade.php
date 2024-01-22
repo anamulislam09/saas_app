62bfea94929efac5ff6ea22c519dbd1d72cb48d9
anamulislam09 <mdanamulislam09@gmail.com>
2024-01-22 18:32:31 +0600

    create side menu

--- resources/views/layouts/admin_partial/sidebar.blade.php
+++ resources/views/layouts/admin_partial/sidebar.blade.php
@@ -1,9 +1,13 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('admin.dashboard')}}" class="brand-link">
-        <img src="{{asset('admin//dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+    <a href="{{ route('admin.dashboard') }}" class="brand-link">
+        <img src="{{ asset('admin//dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Admin dashboard</span>
+        @if (Auth::guard('admin')->user()->role == 0)
+            <span class="brand-text font-weight-light">Super Admin</span>
+        @else
+            <span class="brand-text font-weight-light">Admin dashboard</span>
+        @endif
     </a>
 
     <!-- Sidebar -->
@@ -11,10 +15,11 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{asset('admin//dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('admin//dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                    alt="User Image">
             </div>
             <div class="info">
-                <a href="{{route('admin.dashboard')}}" class="d-block">{{Auth::guard('admin')->user()->name}}</a>
+                <a href="{{ route('admin.dashboard') }}" class="d-block">{{ Auth::guard('admin')->user()->name }}</a>
             </div>
         </div>
         <!-- Sidebar Menu -->
@@ -23,73 +28,100 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
                 @if (Auth::guard('admin')->user()->role == 0)
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
-                        <p>
-                        Customers
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview ml-3">
-                        <li class="nav-item">
-                            <a href="{{route('customers.all')}}" class="nav-link">
-                                <i class="far fa-dot-circle nav-icon"></i>
-                                <p>Customers</p>
-                            </a>
-                        </li>
-                       
-                    </ul>
-                </li>
-                {{-- expense category start here --}}
-                <li class="nav-item">
-                    <a href="{{route('category.index')}}" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
-                        <p>category</p>
-                    </a>
-                </li>
-                {{-- expense category ends here --}}
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                Customers
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview ml-3">
+                            <li class="nav-item">
+                                <a href="{{ route('customers.all') }}" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p>Customers</p>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+                    {{-- expense category start here --}}
+                    <li class="nav-item">
+                        <a href="{{ route('category.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>Expense Category</p>
+                        </a>
+                    </li>
+                    {{-- expense category ends here --}}
                 @endif
+
                 @if (Auth::guard('admin')->user()->role == 1)
-                {{-- Products start here --}}
-                <li class="nav-item">
-                    <a href="{{route('user.index')}}" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
-                        <p>Users</p>
-                    </a>
-                </li>
-                {{-- Products start here --}}
-                {{-- Roles & Parmission start here --}}
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
-                        <p>
-                            Roles & Parmission
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview ml-3">
-                        <li class="nav-item">
-                            <a href="{{route('all.permission')}}" class="nav-link">
-                                <i class="far fa-dot-circle nav-icon"></i>
-                                <p>All Parmission</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="far fa-dot-circle nav-icon"></i>
-                                <p>Subcategory</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="far fa-dot-circle nav-icon"></i>
-                                <p>Brand</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                {{-- Roles & Parmission ends here --}}
+                    {{-- Products start here --}}
+                    <li class="nav-item">
+                        <a href="{{ route('user.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>Users</p>
+                        </a>
+                    </li>
+                    {{-- Products start here --}}
+                    {{-- Roles & Parmission start here --}}
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                Roles & Parmission
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview ml-3">
+                            <li class="nav-item">
+                                <a href="{{ route('all.permission') }}" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p>All Parmission</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    {{-- Roles & Parmission ends here --}}
+                    {{-- Products start here --}}
+                    {{-- Products start here --}}
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-circle"></i>
+                            <p>
+                                Expenses
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview ml-3">
+                            <li class="nav-item">
+                                <a href="" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p>Expense details</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p>Expense</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p>Process & Generete data</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">
+                                    <i class="far fa-dot-circle nav-icon"></i>
+                                    <p> Expense log</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    {{-- Products start here --}}
                 @endif
             </ul>
         </nav>
